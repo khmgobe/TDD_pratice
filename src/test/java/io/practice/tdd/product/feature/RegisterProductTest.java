ab0fe9b14060edd21fcb1d58a8a2bcaf18c4f82c
khmgobe <dr94406@naver.com>
2025-01-18 00:30:04 +0900

    6. create Domain(Product) & validateConstructor

--- src/test/java/io/practice/tdd/product/feature/RegisterProductTest.java
+++ src/test/java/io/practice/tdd/product/feature/RegisterProductTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class RegisterProductTest {
 
 
@@ -23,18 +26,56 @@ class RegisterProductTest {
         final String productDescription = "productDescription";
 
         RegisterProduct.Request request = new RegisterProduct.Request(productName, productDescription);
+
+        registerProduct.request(request);
     }
 
     private static class RegisterProduct {
 
-        final String productName = "productName";
-        final String productDescription = "productDescription";
+        private String productName = "productName";
+        private String productDescription = "productDescription";
+        private RegisterRepository registerRepository;
 
-        public record Request( String productName, String productDescription) {
+        public void request(final Request request) {
+            registerRepository.save(request.toDomain());
+        }
+
+        public record Request(String productName, String productDescription) {
             public Request {
                 Assert.hasText(productName, "제품 이름은 필수입니다.");
                 Assert.hasText(productDescription, "제품 설명은 필수입니다.");
             }
+
+            public Product toDomain() {
+                return new Product(productName, productDescription);
+            }
+        }
+    }
+
+    private static class Product {
+        private final String productName;
+        private final String productDescription;
+
+        public Product(final String productName,
+                       final String productDescription) {
+            this.productName = productName;
+            this.productDescription = productDescription;
+
+            validateConstructor(productName, productDescription);
+        }
+
+        private static void validateConstructor(final String productName, final String productDescription) {
+            Assert.hasText(productName, "제품 이름은 필수입니다.");
+            Assert.hasText(productDescription, "제품 설명은 필수입니다.");
+        }
+    }
+
+    private class RegisterRepository {
+        private final Map<Long, RegisterProduct> registerProducts = new HashMap<>();
+        private Long sequence = 1L;
+
+        public void save(final Product product) {
+
         }
     }
 }
