0313f95019d7b3931e2d44a8a812313f360aefc7
khmgobe <dr94406@naver.com>
2025-01-17 23:58:30 +0900

    init:초기세팅

---
+++ src/main/java/io/practice/tdd/PracticeTddApplication.java
@@ -0,0 +1,13 @@
+package io.practice.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeTddApplication.class, args);
+    }
+
+}
