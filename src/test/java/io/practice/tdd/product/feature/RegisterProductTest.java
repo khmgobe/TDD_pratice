1a2622c5a8b2f7681cfe8e01bfdab0f43111d8a3
khmgobe <dr94406@naver.com>
2025-01-18 00:03:31 +0900

    3. create Request record.

--- src/test/java/io/practice/tdd/product/feature/RegisterProductTest.java
+++ src/test/java/io/practice/tdd/product/feature/RegisterProductTest.java
@@ -1,18 +1,28 @@
 package io.practice.tdd.product.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RegisterProductTest {
 
 
+    private RegisterProduct registerProduct;
+
+    @BeforeEach
+    void setUp() {
+        registerProduct = new RegisterProduct();
+    }
 
     @Test
     @DisplayName("프로덕트를 등록한다")
     void registerProduct()  {
-        RegisterProduct registerProduct = new RegisterProduct();
+        RegisterProduct.Request request = new RegisterProduct.Request();
     }
 
-    private class RegisterProduct {
+    private static class RegisterProduct {
+
+        public record Request() {
+        }
     }
 }
