f814c4c385b0f27a307db4725fff00fd02ae448d
khmgobe <dr94406@naver.com>
2025-01-19 21:22:39 +0900

    5. create class UpdateProduct

--- src/test/java/io/practice/tdd/product/feature/UpdateProductTest.java
+++ src/test/java/io/practice/tdd/product/feature/UpdateProductTest.java
@@ -1,11 +1,12 @@
 package io.practice.tdd.product.feature;
 
+import io.practice.tdd.common.ApiTest;
 import io.practice.tdd.common.Scenario;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
-class UpdateProductTest {
+class UpdateProductTest extends ApiTest {
 
     @Test
     @DisplayName("상품을 수정한다.")
@@ -19,10 +20,13 @@ class UpdateProductTest {
          */
         Scenario.registerProduct().request();
 
+        final Long productId = 1L;
         final String productName = "변경한 상품 이름";
         final String productDescription = "변경한 상품 설명";
 
-        UpdateProductRequest updateProductRequest = new UpdateProductRequest(productName, productDescription);
+        UpdateProductRequest request = new UpdateProductRequest(productName, productDescription);
+
+        UpdateProduct updateProduct = new UpdateProduct();
 
     }
 
@@ -32,5 +36,8 @@ class UpdateProductTest {
             Assert.hasText(productName, "상품명은 필수입니다.");
             Assert.hasText(productDescription, "상품명은 필수입니다.");
         }
+    }
+
+    private class UpdateProduct {
     }
 }
