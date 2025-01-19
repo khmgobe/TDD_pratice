6a65a8f44f9def87eaaf33f1943f9481e89449f2
khmgobe <dr94406@naver.com>
2025-01-19 22:37:27 +0900

    2. Develop Spec 작성 및 Scenario method : (register) 추가.

--- src/test/java/io/practice/tdd/product/feature/DeleteProductTest.java
+++ src/test/java/io/practice/tdd/product/feature/DeleteProductTest.java
@@ -1,4 +1,22 @@
 package io.practice.tdd.product.feature;
 
+import io.practice.tdd.common.Scenario;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class DeleteProductTest {
+
+
+    /**
+     * 1. 상품을 저장한다. [O]
+     * 2. 상품을 삭제한다.
+     * 3. 정상적으로 삭제되었는지 확인한다.
+     */
+
+    @Test
+    @DisplayName("상품을 삭제한다.")
+    void deleteProduct()  {
+
+        Scenario.registerProduct().request();
+    }
 }
