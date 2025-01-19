70ebf8f439d0d4b5b6cc5f91b1641145c7f8a844
khmgobe <dr94406@naver.com>
2025-01-19 22:50:55 +0900

    5. Api 시나리오 테스트를 위한 DeleteProductApi 추가.

--- src/test/java/io/practice/tdd/product/feature/DeleteProductTest.java
+++ src/test/java/io/practice/tdd/product/feature/DeleteProductTest.java
@@ -3,11 +3,14 @@ package io.practice.tdd.product.feature;
 import io.practice.tdd.common.ApiTest;
 import io.practice.tdd.common.Scenario;
 import io.practice.tdd.product.feature.port.ProductPort;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DeleteProductTest extends ApiTest {
 
@@ -25,16 +28,8 @@ class DeleteProductTest extends ApiTest {
     @DisplayName("상품을 삭제한다.")
     void deleteProduct()  {
 
-        // given
-        final Long productId = 1L;
-        Scenario.registerProduct().request();
+        final ExtractableResponse<Response> response = Scenario.deleteProduct().request();
 
-        // when
-        productPort.deleteById(productId);
-
-        // then
-        assertThatThrownBy(() -> productPort.getProduct(productId))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("상품이 존재하지 않습니다.");
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 }
