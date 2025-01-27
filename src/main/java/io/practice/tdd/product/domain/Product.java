bacfeb09ae2deaf87cac6c56cc9235d2e23beeb1
khmgobe <dr94406@naver.com>
2025-01-27 13:27:45 +0900

    4. chore: 도메인 정책 변경으로 인하여 제품 가격, 할인율, 생성시간, 수정시간 컬럼 추가 및 코드 일부 수정.

--- src/main/java/io/practice/tdd/product/domain/Product.java
+++ src/main/java/io/practice/tdd/product/domain/Product.java
@@ -7,11 +7,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.util.Assert;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "products")
-@Comment("프로덕트 테이블")
+@Comment("제품 테이블")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product {
@@ -20,37 +24,79 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_no", nullable = false)
     private Long productNo;
-    @Column(name = "product_name", nullable = false)
+
     @Comment("제품 이름")
+    @Column(name = "product_name", nullable = false)
     private String productName;
+
     @Comment("제품 설명")
     @Column(name = "product_description", nullable = false)
     private String productDescription;
 
+    @Comment("제품 가격")
+    @Column(name = "product_price", nullable = false)
+    private Long productPrice;
+
+    @Comment("제품 할인율")
+    @Column(name = "product_discount")
+    private Integer productDiscount;
+
+    @CreatedDate
+    @Comment("제품 생성시간")
+    @Column(name = "create_at", nullable = false)
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Comment("제품 수정시간")
+    @Column(name = "update_at", nullable = false)
+    private LocalDateTime updateAt;
+
     @Builder
     private Product(final String productName,
-                    final String productDescription) {
+                    final String productDescription,
+                    final Long productPrice) {
 
-        validateConstructor(productName, productDescription);
+        validateConstructor(productName, productDescription, productPrice);
         this.productName = productName;
         this.productDescription = productDescription;
+        this.productPrice = productPrice;
 
     }
 
-    private void validateConstructor(final String productName, final String productDescription) {
+    private void validateConstructor(final String productName, final String productDescription, final Long productPrice) {
         Assert.hasText(productName, "제품 이름은 필수입니다.");
         Assert.hasText(productDescription, "제품 설명은 필수입니다.");
+        Assert.notNull(productPrice, "제품의 가격은 필수입니다.");
+
+        validateProductPrice(productPrice);
+
     }
 
-    public void update(final String productName, final String productDescription) {
+    private static void validateProductPrice(final Long productPrice) {
+        if (0 > productPrice) {
+            throw new IllegalArgumentException("제품의 가격은 1원 이상이어야 합니다.");
+        }
+    }
+
+    public void update(final String productName, final String productDescription, final Long productPrice) {
         this.productName = productName;
         this.productDescription = productDescription;
-        Assert.hasText(productName, "상품명은 필수입니다.");
-        Assert.hasText(productDescription, "상품명은 필수입니다.");
+        this.productPrice = productPrice;
+        Assert.hasText(productName, "제품명은 필수입니다.");
+        Assert.hasText(productDescription, "제품명은 필수입니다.");
+        Assert.notNull(productPrice, "제품 가격");
     }
 
     public GetProductResponse toProductResponse(final Product product) {
-        final GetProductResponse response = new GetProductResponse(product.getProductNo(), product.getProductName(), product.getProductDescription());
+
+        final GetProductResponse response = new GetProductResponse(
+                product.getProductNo(),
+                product.getProductName(),
+                product.getProductDescription(),
+                product.getProductPrice(),
+                product.getCreateAt(),
+                product.getUpdateAt());
+
         return response;
     }
 }
