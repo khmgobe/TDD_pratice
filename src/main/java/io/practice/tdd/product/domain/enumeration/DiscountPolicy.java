f4c302314d27de0c5569788dc7605afe7486f7b1
khmgobe <dr94406@naver.com>
2025-01-28 15:09:05 +0900

    9. feat: 할인 정책에 따른 금액 계산을 하는 메서드 추가.

--- src/main/java/io/practice/tdd/product/domain/enumeration/DiscountPolicy.java
+++ src/main/java/io/practice/tdd/product/domain/enumeration/DiscountPolicy.java
@@ -2,6 +2,19 @@ package io.practice.tdd.product.domain.enumeration;
 
 public enum DiscountPolicy {
 
-    NONE,
-    FIXED_AMOUNT,
+    NONE {
+        @Override
+        public Long apply(final Long productPrice) {
+            return productPrice;
+        }
+    },
+
+    FIXED_1000_AMOUNT {
+        @Override
+        public Long apply(final Long productPrice) {
+            return Math.max(productPrice - 1000, 0L);
+        }
+    };
+
+    public abstract Long apply(final Long productPrice);
 }
