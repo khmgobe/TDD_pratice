5077181f587dec6b5f27155efeb94a0df20f70de
khmgobe <dr94406@naver.com>
2025-01-28 23:24:29 +0900

    2. feat: RegisterOrder 내부 클래스 생성 및 주문 등록 테스트케이스 생성.

--- src/test/java/io/practice/tdd/order/RegisterOrderTest.java
+++ src/test/java/io/practice/tdd/order/RegisterOrderTest.java
@@ -1,6 +1,18 @@
 package io.practice.tdd.order;
 
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RegisterOrderTest {
 
+
+    @Test
+    @DisplayName("상품의 데이터로 주문을 등록한다.")
+    void registerOrder()  {
+        RegisterOrder registerOrder = new RegisterOrder();
+    }
+
+    private class RegisterOrder {
+    }
 }
