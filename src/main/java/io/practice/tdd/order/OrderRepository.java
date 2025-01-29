7aab40aeb20e27acbf115a416fa831815a4275d5
khmgobe <dr94406@naver.com>
2025-01-29 18:30:36 +0900

    9. feat: Pojo Test -> API Test 기반으로 변경 및 확인.

--- src/main/java/io/practice/tdd/order/OrderRepository.java
+++ src/main/java/io/practice/tdd/order/OrderRepository.java
@@ -1,10 +1,13 @@
 package io.practice.tdd.order;
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Component
 class OrderRepository {
 
     private final Map<Long, Order> orderMap = new HashMap<>();
--- src/test/java/io/practice/tdd/order/RegisterOrderTest.java
+++ src/test/java/io/practice/tdd/order/RegisterOrderTest.java
@@ -1,25 +1,23 @@
 package io.practice.tdd.order;
 
 
+import io.practice.tdd.common.ApiTest;
 import io.practice.tdd.product.domain.Product;
 import io.practice.tdd.product.util.ProductSteps;
-import org.junit.jupiter.api.BeforeEach;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class RegisterOrderTest {
+class RegisterOrderTest extends ApiTest {
 
-
-    private RegisterOrder registerOrder;
+    @Autowired
     private OrderRepository orderRepository;
-
-    @BeforeEach
-    void setUp() {
-        orderRepository = new OrderRepository();
-        registerOrder = new RegisterOrder(orderRepository);
-    }
 
     @Test
     @DisplayName("상품의 데이터로 주문을 등록한다.")
@@ -29,10 +27,18 @@ class RegisterOrderTest {
         final Product product = ProductSteps.productBuilder();
         final int quantity = 5;
 
-        RegisterOrderRequest orderRequest = new RegisterOrderRequest(product, quantity);
+        RegisterOrderRequest request = new RegisterOrderRequest(product, quantity);
 
-        registerOrder.register(orderRequest);
+        final ValidatableResponse response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/orders")
+                .then()
+                .log().all()
+                .statusCode(HttpStatus.CREATED.value());
 
         assertThat(orderRepository.findAll()).hasSize(1);
+
     }
 }
