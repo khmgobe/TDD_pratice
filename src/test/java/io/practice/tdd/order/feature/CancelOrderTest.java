78014a8d4aba91c355b33129fa035d6396e779dd
khmgobe <dr94406@naver.com>
2025-01-31 22:39:48 +0900

    3. feat: FakeOrder(도메인 객체) 추가.

--- src/test/java/io/practice/tdd/order/feature/CancelOrderTest.java
+++ src/test/java/io/practice/tdd/order/feature/CancelOrderTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 class CancelOrderTest {
 
-
     private CancelOrderService cancelOrderService;
     private FakeOrderRepository fakeOrderRepository;
 
@@ -19,6 +18,7 @@ class CancelOrderTest {
     @DisplayName("주문을 취소한다.")
     void cancelOrder()  {
         cancelOrderService.cancelOrder();
+        FakeOrder fakeOrder = new FakeOrder(1L, "orderName", "orderDescription");
     }
 
     private class CancelOrderService {
@@ -29,5 +29,19 @@ class CancelOrderTest {
     }
 
     private class FakeOrderRepository {
+
+    }
+
+    private class FakeOrder {
+
+        final long id;
+        final String orderName;
+        final String orderDescription;
+
+        public FakeOrder(final long id, final String orderName, final String orderDescription) {
+        this.id = id;
+        this.orderName = orderName;
+        this.orderDescription = orderDescription;
+        }
     }
 }
