6f3ba70b29ef015b50291fbd5a04189b20c12722
khmgobe <dr94406@naver.com>
2025-01-31 23:00:38 +0900

    11. feat: CancelOrderService 내부 클래스를 생성한다.

--- src/test/java/io/practice/tdd/order/feature/CancelOrderTest.java
+++ src/test/java/io/practice/tdd/order/feature/CancelOrderTest.java
@@ -3,7 +3,7 @@ package io.practice.tdd.order.feature;
 import io.practice.tdd.common.ApiTest;
 import io.practice.tdd.common.Scenario;
 import io.practice.tdd.order.out.persistence.OrderRepository;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,17 +12,28 @@ class CancelOrderTest extends ApiTest {
 
     @Autowired
     private OrderRepository orderRepository;
+    private CancelOrderService cancelOrderService;
+
+    @BeforeEach
+    void setUp() {
+        cancelOrderService = new CancelOrderService();
+    }
 
     @Test
     @DisplayName("주문을 등록하고 등록한 주문을 취소한다.")
     void cancelOrder() {
         final Long orderId = 1L;
 
-
         // given: 상품을 생성하고 상품 정보 기반으로 주문을 등록한다.
         Scenario.registerProduct().request();
         Scenario.registerOrder().request();
 
-        Assertions.assertThat(orderRepository.findAll()).hasSize(1);
+        // when:
+
+
+
+    }
+
+    private class CancelOrderService {
     }
 }
