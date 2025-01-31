768440bdb928a967e181948a3fa12daf31c6a7a9
khmgobe <dr94406@naver.com>
2025-01-31 22:59:31 +0900

    10. feat: ApiTest, orderRepository 의존성 등록 후 상품, 주문 등록의 시나리오가 정상적으로 동작하는지 확인한다.

--- src/test/java/io/practice/tdd/order/feature/CancelOrderTest.java
+++ src/test/java/io/practice/tdd/order/feature/CancelOrderTest.java
@@ -1,7 +1,9 @@
 package io.practice.tdd.order.feature;
 
 import io.practice.tdd.common.ApiTest;
+import io.practice.tdd.common.Scenario;
 import io.practice.tdd.order.out.persistence.OrderRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +13,16 @@ class CancelOrderTest extends ApiTest {
     @Autowired
     private OrderRepository orderRepository;
 
-
     @Test
     @DisplayName("주문을 등록하고 등록한 주문을 취소한다.")
     void cancelOrder() {
-
         final Long orderId = 1L;
+
+
+        // given: 상품을 생성하고 상품 정보 기반으로 주문을 등록한다.
+        Scenario.registerProduct().request();
+        Scenario.registerOrder().request();
+
+        Assertions.assertThat(orderRepository.findAll()).hasSize(1);
     }
 }
