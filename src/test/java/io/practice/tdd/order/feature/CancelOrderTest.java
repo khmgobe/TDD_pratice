b44e4d9ba2e820827439453cd0c3fb8b7ccfde31
khmgobe <dr94406@naver.com>
2025-01-31 22:58:02 +0900

    9. feat: 가짜 테스트에 사용했던 코드 제거 및 ApiTest로 전환과 orderRepository 의존성 추가.

--- src/test/java/io/practice/tdd/order/feature/CancelOrderTest.java
+++ src/test/java/io/practice/tdd/order/feature/CancelOrderTest.java
@@ -1,112 +1,21 @@
 package io.practice.tdd.order.feature;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import io.practice.tdd.common.ApiTest;
+import io.practice.tdd.order.out.persistence.OrderRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+class CancelOrderTest extends ApiTest {
 
-class CancelOrderTest {
+    @Autowired
+    private OrderRepository orderRepository;
 
-    private CancelOrderService cancelOrderService;
-    private FakeOrderRepository fakeOrderRepository;
-
-    @BeforeEach
-    void setUp() {
-        fakeOrderRepository = new FakeOrderRepository();
-        cancelOrderService = new CancelOrderService(fakeOrderRepository);
-    }
 
     @Test
     @DisplayName("주문을 등록하고 등록한 주문을 취소한다.")
     void cancelOrder() {
 
         final Long orderId = 1L;
-        final String orderName = "orderName";
-        final String orderDescription = "orderDescription";
-
-        FakeOrderRequest fakeOrderRequest = new FakeOrderRequest(orderName, orderDescription);
-
-        cancelOrderService.registerOrder(fakeOrderRequest);
-
-        cancelOrderService.cancelOrder(orderId);
-
-        Assertions.assertThat(fakeOrderRepository.findAll()).size().isEqualTo(0);
-    }
-
-    private class CancelOrderService {
-
-        private final FakeOrderRepository fakeOrderRepository;
-
-        public CancelOrderService(final FakeOrderRepository fakeOrderRepository) {
-            this.fakeOrderRepository = fakeOrderRepository;
-        }
-
-        public void registerOrder(FakeOrderRequest fakeOrderRequest) {
-            final FakeOrder domain = fakeOrderRequest.toDomain();
-            fakeOrderRepository.save(domain);
-        }
-
-        public void cancelOrder(final Long orderId) {
-            fakeOrderRepository.deleteById(orderId);
-        }
-    }
-
-    private class FakeOrderRepository {
-
-        private Map<Long, FakeOrder> fakeOrderMap = new HashMap<>();
-        private Long sequence = 1L;
-
-        public void save(final FakeOrder fakeOrder) {
-            fakeOrder.assignId(sequence++);
-            fakeOrderMap.put(fakeOrder.getId(), fakeOrder);
-        }
-
-        public List<FakeOrder> findAll() {
-            return new ArrayList<>(fakeOrderMap.values());
-        }
-
-        public void deleteById(final Long orderId) {
-            fakeOrderMap.remove(orderId);
-        }
-    }
-
-    private static class FakeOrder {
-
-        private Long id;
-        private final String orderName;
-        private final String orderDescription;
-
-        public FakeOrder( final String orderName, final String orderDescription) {
-            this.orderName = orderName;
-            this.orderDescription = orderDescription;
-        }
-
-        public void assignId(final Long id) {
-            this.id = id;
-        }
-
-        public Long getId() {
-            return id;
-        }
-    }
-
-    private record FakeOrderRequest(
-            String orderName,
-            String orderDescription) {
-
-        public FakeOrderRequest {
-            Assert.notNull(orderName, "주문 이름은 필수입니다.");
-            Assert.notNull(orderDescription, "주문 정보는 필수입니다.");
-        }
-
-        public FakeOrder toDomain() {
-            return new FakeOrder(orderName, orderDescription);
-        }
     }
 }
