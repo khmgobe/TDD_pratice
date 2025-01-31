1ec0516a67698e7c6bdb4289f5b35818f0ea92fc
khmgobe <dr94406@naver.com>
2025-01-31 23:42:19 +0900

    16. chore: 주문 취소 서비스 | 컨트롤러 분리 리팩토링.

--- src/main/java/io/practice/tdd/order/application/CancelOrderService.java
+++ src/main/java/io/practice/tdd/order/application/CancelOrderService.java
@@ -2,17 +2,17 @@ package io.practice.tdd.order.application;
 
 import io.practice.tdd.order.out.persistence.OrderRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Service
 @RequiredArgsConstructor
 public class CancelOrderService {
 
     private final OrderRepository orderRepository;
 
-    @DeleteMapping("/orders/{orderId}")
+    @Transactional
     public void cancelOrder(@PathVariable final Long orderId) {
         orderRepository.deleteById(orderId);
     }
