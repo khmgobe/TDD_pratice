30b74337640741c47f280fa9f85e231ff8ce0b89
khmgobe <dr94406@naver.com>
2025-01-31 22:35:13 +0900

    2. feat: 테스트 내 CancelOrderService 내부클래스 생성.

--- src/test/java/io/practice/tdd/order/feature/CancelOrderTest.java
+++ src/test/java/io/practice/tdd/order/feature/CancelOrderTest.java
@@ -1,14 +1,25 @@
 package io.practice.tdd.order.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CancelOrderTest {
 
 
+    private CancelOrderService cancelOrderService;
+
+    @BeforeEach
+    void setUp() {
+        cancelOrderService = new CancelOrderService();
+    }
+
     @Test
     @DisplayName("주문을 취소한다.")
     void cancelOrder()  {
 
+    }
+
+    private class CancelOrderService {
     }
 }
