85bf5277a09a40c6fa61a96cef02e80574d47109
khmgobe <dr94406@naver.com>
2025-01-31 22:34:00 +0900

    1. feat: CancelOrderTest 생성 및 testCase 생성.

---
+++ src/test/java/io/practice/tdd/order/feature/CancelOrderTest.java
@@ -0,0 +1,14 @@
+package io.practice.tdd.order.feature;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CancelOrderTest {
+
+
+    @Test
+    @DisplayName("주문을 취소한다.")
+    void cancelOrder()  {
+
+    }
+}
